760444c311a2603652c364f8291be4e358829355
Shiva Kumar Chakali <schakali@charlotte.edu>
2025-04-27 00:27:30 -0400

    Dynamic working model

--- app.py
+++ app.py
@@ -24,19 +24,25 @@ if st.button("Generate Video"):
         os.makedirs("demo_audio", exist_ok=True)
 
         # Save uploaded audio file with a unique name to avoid overwrites
-        unique_filename = f"{uuid.uuid4().hex}_{audio_file.name}"
-        audio_path = os.path.join("demo_audio", unique_filename)
+        unique_filename = f"{audio_file.name}"
+        audio_path = os.path.join("./demo_audio", unique_filename)
         # audio_path = './demo_audio/1st-page.wav'
-        with open(audio_path, "wb") as f:
-            f.write(audio_file.read())
-
         # Construct command
         if demo_type == "Whole Body":
-            cmd = f"python scripts/demo.py --config_file ./config/body_pixel.json --infer --audio_file ./demo_audio/1st-page.wav --id 0 --whole_body"
+            cmd = f"python scripts/demo.py --config_file ./config/LS3DCG.json --infer --audio_file {audio_path} --body_model_name s2g_LS3DCG --body_model_path experiments/2022-10-19-smplx_S2G-LS3DCG/ckpt-99.pth --id 0"
+            base_name = os.path.splitext(os.path.basename(audio_path))[0]
+            video_dir = os.path.join("visualise", "video","LS3DCG", f"{base_name}.wav")
+            video_path = os.path.join(video_dir, f"{base_name}.mp4")
         elif demo_type == "Only Face":
-            cmd = f"python scripts/demo.py --config_file ./config/body_pixel.json --infer --audio_file ./demo_audio/1st-page.wav --id 0 --only_face"
+            cmd = f"python scripts/demo.py --config_file ./config/body_pixel.json --infer --audio_file {audio_path} --id 0 --only_face"
+            base_name = os.path.splitext(os.path.basename(audio_path))[0]
+            video_dir = os.path.join("visualise", "video","body-pixel2", f"{base_name}.wav")
+            video_path = os.path.join(video_dir, f"{base_name}.mp4")
         elif demo_type == "Diversity (Multiple Samples)":
-            cmd = f"python scripts/demo.py --config_file ./config/body_pixel.json --infer --audio_file ./demo_audio/1st-page.wav --id 0 --num_sample 12"
+            cmd = f"python scripts/demo.py --config_file ./config/body_pixel.json --infer --audio_file {audio_path} --id 0 --num_sample 12"
+            base_name = os.path.splitext(os.path.basename(audio_path))[0]
+            video_dir = os.path.join("visualise", "video","body-pixel2", f"{base_name}.wav")
+            video_path = os.path.join(video_dir, f"{base_name}.mp4")
 
         print(audio_path)
         # Run the command
@@ -54,10 +60,11 @@ if st.button("Generate Video"):
         process.wait()
 
         # Try to locate generated video
-        base_name = os.path.splitext(os.path.basename(audio_path))[0]
-        video_dir = os.path.join("visualise", "video", "body-pixel2", f"{base_name}.mp4")
+        # base_name = os.path.splitext(os.path.basename(audio_path))[0]
+        # video_dir = os.path.join("visualise", "video","LS3DCG", "1st-page.wav", f"{base_name}.mp4")
         # video_path = os.path.join(video_dir, f"{base_name}.mp4")
-        video_path = 'visualise/video/body-pixel2/style.wav/style.mp4'
+        # video_path = 'visualise/video/LS3DCG/1st-page.wav/1st-page.mp4'
+        print(video_path)
 
         if os.path.exists(video_path):
             st.success("Video generated successfully!")
